a9c62e807e9a9f14edd0f421cc68f8a39c9e398d
Maksim <maksim.kopylov.x5@gmail.com>
2021-03-31 16:36:38 +0300

    delete committttttttt

--- src/main/java/max_sk/SpringApplication.java
+++ src/main/java/max_sk/SpringApplication.java
@@ -7,12 +7,6 @@ public class SpringApplication {
         for (int i = 0; i < 10; i++) {
             System.out.println(hello);
         }
-
-        int j = 0;
-        for (int i = 0; i <10; i++){
-            j = j + i++;
-            System.out.println(j);
-        }
-
+        
     }
 }
